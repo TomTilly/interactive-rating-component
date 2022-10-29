37cb72cff23c7eee14425e3901c4c16f57602aef
Tom Tillistrand <tomtillistrand@gmail.com>
2022-10-28 21:58:55 -0400

    Clean up and comment

--- src/scripts/Rating.ts
+++ src/scripts/Rating.ts
@@ -1,35 +1,30 @@
-import { setAttribute, removeAttribute } from './utilities.js';
-
 /**
- * The Interactive Rating component expects the following HTML structure:
+ * Updates interactive rating HTML on submit
+ *
+ * @example
+ *
+ * ## HTML expected
+ * ```html
  * <div class="interactive-rating">
- *   <form>
- *     <div class="stack-vertical">
- *       <label class="interactive-rating__option">
- *         <input type="radio" name="rating" value="1" />
- *           1
- *        </label>
- *        <!-- repeat x times...
- *     </div>
- *     <button type="submit">
- *       Submit
- *     </button>
- *   </form>
+ *  <form>
+ *    <div class="stack-vertical">
+ *      <label class="interactive-rating__option">
+ *        <input type="radio" name="rating" value="1" />
+ *        1
+ *      </label>
+ *      <!-- repeat x times... -->
+ *    </div>
+ *    <button type="submit">
+ *      Submit
+ *    </button>
+ *  </form>
  * </div>
+ * ```
  */
-
-// const ratingOptions = ['1', '2', '3', '4', '5'] as const;
-// type Rating = typeof ratingOptions[number];
-
-// function isInput(maybeInput: any): maybeInput is HTMLInputElement {
-//   return maybeInput instanceof HTMLInputElement && maybeInput?.type === 'radio';
-// }
-
 class Rating {
   private root: HTMLElement;
   private radioEls: HTMLInputElement[];
   private formEl: HTMLFormElement;
-  private _isValid: boolean | undefined;
 
   constructor(root: HTMLElement) {
     const radioEls = root.querySelectorAll<HTMLInputElement>(
@@ -37,15 +32,15 @@ class Rating {
     );
     const formEl = root.querySelector('form');
 
-    // Basic validation to ensure HTML is somewhat well-formed
     if (!formEl) throw new Error('No form found');
 
     this.root = root;
     this.radioEls = [...radioEls];
     this.formEl = formEl;
 
+    // Event handlers
     this.formEl.addEventListener('submit', this.handleSubmit);
-    this.root.addEventListener('animationend', (e) => {
+    this.root.addEventListener('animationend', () => {
       this.root.removeAttribute('data-invalid');
     });
   }
@@ -57,15 +52,15 @@ class Rating {
     const formData = new FormData(formEl);
     const maybeRating = formData.get('rating');
 
+    // No rating was selected, show error
     if (maybeRating === null) {
       this.showValidationError('Error: no rating was selected.');
-      this.isValid = false;
       return;
     }
-    // Rating is set
-    const rating = maybeRating;
-    this.isValid = true;
 
+    // Rating was selected, show success message
+    const rating = maybeRating.toString();
+    this.root.removeAttribute('data-invalid');
     root.innerHTML = `
       <div class="stack-vertical align-center">
         <img class="interative-rating__thank-you-img" src="assets/illustration-thank-you.svg" alt="">
@@ -76,47 +71,36 @@ class Rating {
     `;
   };
 
-  // Used to notify screen readers of errors
   showValidationError = (msg: string) => {
-    const { formEl, radioEls } = this;
-    // Clear previous validation error
-    const maybeValidationEl = formEl.querySelector(
-      '.interactive-rating__error'
-    );
-    if (maybeValidationEl) maybeValidationEl.remove();
+    const { formEl, radioEls, root } = this;
 
-    // Show new validation error
+    // Clear previous validation error
+    formEl.querySelector('.interactive-rating__error')?.remove();
+
+    // Figure out which classes to use
+    const classes: string[] = ['interactive-rating__error', 'color-orange'];
     const prefersReducedMotion = window.matchMedia(
       '(prefers-reduced-motion: reduce)'
     ).matches;
-    const validationEl = document.createElement('div');
-    const classes: string[] = ['interactive-rating__error', 'color-orange'];
     if (!prefersReducedMotion) classes.push('sr-only');
+
+    // Create validation element
+    const validationEl = document.createElement('div');
     validationEl.classList.add(...classes);
     validationEl.textContent = msg;
     validationEl.id = 'error';
 
+    // Add validation message to page
     const stackEl = formEl.querySelector('.stack-vertical');
     if (!stackEl) throw new Error('No ".stack-vertical" element found.');
     stackEl.insertAdjacentElement('afterbegin', validationEl);
 
+    // Set invalid attributes
+    root.setAttribute('data-invalid', '');
     for (const radioEl of radioEls) {
       radioEl.setAttribute('aria-invalid', 'error');
     }
   };
-
-  get isValid() {
-    return this._isValid;
-  }
-
-  set isValid(value) {
-    if (value === true) {
-      this.root.removeAttribute('data-invalid');
-    } else {
-      this.root.setAttribute('data-invalid', '');
-    }
-    this._isValid = value;
-  }
 }
 
 export default Rating;
